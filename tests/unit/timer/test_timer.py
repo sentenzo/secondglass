d8bf9e52e72214c694a535f3e6e7dda7eb138b48
sentenzo <sentenzo@users.noreply.github.com>
2024-02-11 10:02:10 +0300

    `timer` tests: `TICKING` status checks

--- tests/unit/timer/test_timer.py
+++ tests/unit/timer/test_timer.py
@@ -3,7 +3,7 @@ from time import sleep
 import pytest
 
 from secondglass.timer.exceptions import TimerInvalidAction, TimerValueError
-from secondglass.timer.msec import Milliseconds
+from secondglass.timer.msec import MSEC_IN_SEC, Milliseconds
 from secondglass.timer.timer import DEFAULT_DURATION, Status, Timer
 
 
@@ -42,7 +42,6 @@ def test_idle_transitions() -> None:
     # invalid actions check
     for action in (timer.stop, timer.restart, timer.pause, timer.resume):
         with pytest.raises(TimerInvalidAction):
-            print(action)
             action()
 
     # passive tick check
@@ -61,3 +60,72 @@ def test_idle_transitions() -> None:
     assert timer.init_duration == timer.duration_left == new_duration
     assert timer.status == Status.TICKING
     assert timer.time_since_rang is None
+
+
+def test_ticking_transitions() -> None:
+    # init
+    small_duration = Milliseconds(100)  # 0.1 sec
+    timer = Timer()
+    timer.start(small_duration)
+
+    # invalid actions check
+    for action in (timer.start, timer.resume):
+        with pytest.raises(TimerInvalidAction):
+            action()  # type: ignore
+
+    # tick action
+    assert timer.init_duration == timer.duration_left
+    timer.tick()
+    sleep(0.01)
+    timer.tick()
+    assert timer.init_duration > timer.duration_left
+    assert timer.status == Status.TICKING
+    assert timer.time_since_rang is None
+    sleep(small_duration / MSEC_IN_SEC)
+    timer.tick()  # transmission into RANG
+    assert timer.status == Status.RANG
+    assert timer.time_since_rang is not None
+    assert timer.duration_left == Milliseconds(0)
+
+    # re-init
+    timer = Timer()
+    timer.start(small_duration)
+
+    # pause action
+    timer.tick()
+    assert timer.last_tick_time is not None
+    timer.pause()
+    assert timer.status == Status.PAUSED
+    assert timer.last_tick_time is None
+
+    # re-init
+    timer = Timer()
+    timer.start(small_duration)
+
+    # restart action
+    timer.tick()
+    sleep(0.01)
+    timer.tick()
+    assert timer.init_duration > timer.duration_left
+    assert timer.last_tick_time is not None
+    assert timer.status == Status.TICKING
+    timer.restart()
+    assert timer.init_duration == timer.duration_left
+    assert timer.last_tick_time is None
+    assert timer.status == Status.TICKING
+
+    # re-init
+    timer = Timer()
+    timer.start(small_duration)
+
+    # stop action
+    timer.tick()
+    sleep(0.01)
+    timer.tick()
+    assert timer.init_duration > timer.duration_left
+    assert timer.last_tick_time is not None
+    assert timer.status == Status.TICKING
+    timer.stop()
+    assert timer.init_duration == timer.duration_left
+    assert timer.last_tick_time is None
+    assert timer.status == Status.IDLE
