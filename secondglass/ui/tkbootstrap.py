bab644aa8c45457a77963f7fd1826b81e2be1981
sentenzo <sentenzo@users.noreply.github.com>
2024-02-14 16:11:23 +0300

    `ttkbootstrap` visual improvement

--- secondglass/ui/tkbootstrap.py
+++ secondglass/ui/tkbootstrap.py
@@ -1,7 +1,8 @@
-from tkinter import Misc
+import tkinter as tk
+from tkinter.font import Font
 
-import ttkbootstrap as ttk
-from ttkbootstrap.constants import BOTH, CENTER, TOP, YES
+import ttkbootstrap as tb
+import ttkbootstrap.constants as c
 
 from secondglass.helpers import pyinstaller_fix_path
 from secondglass.progress import IProgressIndicator, ProgressIndicator
@@ -10,47 +11,92 @@ from secondglass.timer import Status, Timer
 from .ui import UI
 
 
-class MyAwesomeApp(ttk.Frame):
-    def __init__(self, master: Misc | None = None) -> None:
+class MyAwesomeApp(tb.Frame):
+    def __init__(self, master: tk.Misc | None = None) -> None:
         super().__init__(master)
         self.status: Status = Status.IDLE
 
-        self.pack(fill=BOTH, expand=YES)
+        self.pack(
+            fill=c.BOTH,
+            expand=c.YES,
+        )
 
-        self.running = ttk.BooleanVar(value=False)
-        self.afterid = ttk.StringVar()
-        self.elapsed = ttk.IntVar()
-        self.timer_text = ttk.StringVar(value="00:00:00")
+        self.running = tb.BooleanVar(value=False)
+        self.afterid = tb.StringVar()
+        self.elapsed = tb.IntVar()
+        self.timer_text = tb.StringVar(value="5 minutes")
 
         self.create_layout()
+        # self.font = ("Helvetica", 126)
 
-    def create_layout(self) -> None:
-        """Create the stopwatch number display"""
-        lbl = ttk.Label(
-            master=self,
-            font="-size 32",
-            anchor=CENTER,
+    def create_input_box(self) -> tb.Entry:
+        entry = tb.Entry(
+            self,
             textvariable=self.timer_text,
-            # background="#f00",
+            justify=c.CENTER,
+            font=Font(size=16),
         )
-        lbl.pack(side=TOP, fill=BOTH, padx=0, pady=0)
-        pbar = ttk.Progressbar(
-            master=self,
+        return entry
+
+    def create_buttons(self) -> tb.Frame:
+        btn_frame = tb.Frame(
+            self,
+        )
+        btn_start = tb.Button(btn_frame, text="start", bootstyle=c.PRIMARY)
+        btn_restart = tb.Button(btn_frame, text="restart", bootstyle=c.WARNING)
+        btn_pause = tb.Button(btn_frame, text="pause", bootstyle=c.SECONDARY)
+        btn_resume = tb.Button(btn_frame, text="resume", bootstyle=c.PRIMARY)
+        btn_stop = tb.Button(btn_frame, text="stop", bootstyle=c.WARNING)
+        btn_start.pack(side=c.LEFT, padx=2, pady=2)
+        btn_restart.pack(side=c.LEFT, padx=2, pady=2)
+        btn_pause.pack(side=c.LEFT, padx=2, pady=2)
+        btn_resume.pack(side=c.LEFT, padx=2, pady=2)
+        btn_stop.pack(side=c.LEFT, padx=2, pady=2)
+        return btn_frame
+
+    def create_progress_bar(self) -> tb.Progressbar:
+        pbar = tb.Progressbar(
+            self,
             maximum=1.0,
             value=0.35,
-            bootstyle="success",
-            phase=1,
+            bootstyle=c.DEFAULT,
+            # phase=1,
         )
-        pbar.pack(side=TOP, fill=BOTH, padx=0, pady=0)
-        pbar.start()
+        # pbar.start()
+        return pbar
+
+    def create_layout(self) -> None:
+        ibox = self.create_input_box()
+        ibox.pack(
+            # anchor=N,
+            side=c.TOP,
+            fill=c.X,
+            expand=c.YES,
+            padx=8,
+            pady=(2, 1),
+        )
+
+        btn_frame = self.create_buttons()
+        btn_frame.pack()
+
+        pbar = self.create_progress_bar()
+        pbar.pack(
+            # anchor=S,
+            # side=BOTTOM,
+            fill=c.X,
+            expand=c.NO,
+            padx=(2, 2),
+            pady=(1, 2),
+            ipady=1,
+        )
 
 
 class TkbUI(UI):
     def __init__(self, timer: Timer) -> None:
         icon_path = pyinstaller_fix_path("secondglass/rec/clock.png")
-        self.app = ttk.Window(
+        self.app = tb.Window(
             title="MyAwesomeApp",
-            themename="cosmo",
+            themename="minty",
             minsize=(240, 64),
             iconphoto=icon_path,
             # resizable=(False, False),
