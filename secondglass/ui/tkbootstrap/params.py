b72f7ee72b26bb14a21bc35d2fdcb2241459d996
sentenzo <sentenzo@users.noreply.github.com>
2024-02-19 08:39:52 +0300

    better size evaluation

--- secondglass/ui/tkbootstrap/params.py
+++ secondglass/ui/tkbootstrap/params.py
@@ -7,11 +7,12 @@ from secondglass.timer import Timer
 UI_THEME = "simplex"
 WINDOW_MIN_SIZE = (280, 120)
 WINDOW_INIT_SIZE = (400, 180)
-FONT_INIT_SIZE = 16
+FONT_INIT_SIZE = 17
 FONT_FAMILY = "Areal"
 PADDING = 10
 RENDER_DELAY_MS = 30  # 0.03 sec
 INIT_TEXT = "5 minutes"
+WH_RATIO_THRESHOLD = 2.6
 
 
 @dataclass
@@ -25,8 +26,16 @@ class Params:
     )
     size: tb.DoubleVar = field(default_factory=lambda: tb.DoubleVar(value=1.0))
 
+    @staticmethod
+    def _calc_size(width: int, height: int) -> float:
+        wh_ratio_threshold = WH_RATIO_THRESHOLD
+        wh_ratio = width / height
+        if wh_ratio > wh_ratio_threshold:
+            width = int(height * wh_ratio_threshold)
+        return width / WINDOW_INIT_SIZE[0]
+
     def update_size(self, current_size: tuple[int, int]) -> None:
         old_size = self.size.get()
-        new_size = current_size[0] / WINDOW_INIT_SIZE[0]
+        new_size = self._calc_size(*current_size)
         if old_size != new_size:
             self.size.set(new_size)
